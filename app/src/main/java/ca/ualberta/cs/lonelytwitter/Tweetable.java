d992ee31b22c87a7318d4ed13b9d932a97676340
MitchellBallou <ballou@ualberta.ca>
2017-09-29 12:50:17 -0600

    Added comments

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -5,7 +5,9 @@
  *
  * Date: September 28, 2017
  *
- * Copyright (c) Team X, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behavior at University of Alberta
+ * Copyright (c) Team X, CMPUT301, University of Alberta - All Rights Reserved. You may use,
+ * distribute, or modify this code under terms and conditions of the Code of Students Behavior at
+ * University of Alberta
  */
 
 package ca.ualberta.cs.lonelytwitter;
